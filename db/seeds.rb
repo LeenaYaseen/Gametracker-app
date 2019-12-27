62d3e0b11f6223c1f20f61afbd3043e0be2a55b8
Leena Yaseen <leena4.y@gmail.com>
2019-12-27 16:43:59 +0300

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create(name: "Square Enix", platform: "Playstation 4", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Square_Enix_logo.svg/760px-Square_Enix_logo.svg.png",user_id:1)
-Company.create(name: "NIS America", platform: "Playstation 4", logo: "https://thumbnails.pcgamingwiki.com/a/aa/NIS_America_-_Logo.png/300px-NIS_America_-_Logo.png",user_id:1)
-
-
-Game.create(company_id: 1, title:"Final Fantasy XV", genre: "Action-Adventure", cover:"https://vignette.wikia.nocookie.net/finalfantasy/images/2/25/FFXV_logo.png",status:true)
-Game.create(company_id: 2, title:"Disgaea 5", genre: "Tactical RPG", cover:"https://img.favpng.com/8/23/20/disgaea-5-disgaea-4-playstation-4-video-game-nippon-ichi-software-png-favpng-4rQaVRBQu5PfjXCfmGFLcTL74.jpg",status:true)
